bf7a78274110a16879a585b1d65e2cceeab2ba59
Artur <artur.rudzik@o2.pl>
2021-11-09 18:00:33 +0100

    marginesy kalendarz

--- GOCC/ViewModel/CalendarPageViewModel.cs
+++ GOCC/ViewModel/CalendarPageViewModel.cs
@@ -24,7 +24,6 @@ namespace GOCC.ViewModel
             {
                 new TableCalendarData() {Time = "9:00 - 12:00", Name = "Bieg dzieci dla WOŚP", Localization = "MORiS ul. Lompy 10 A"},
                 new TableCalendarData() {Time = "12:00 - 18:00", Name = "VII. Bieg z sercem WOŚP", Localization = "MORiS ul. Lompy 10 A"},
-                new TableCalendarData() {Time = "12:00 - 18:00", Name = "Mecz hokeja", Localization = "Lodowisko Chorzów Rynek"},
                 new TableCalendarData() {Time = "14:00 - 18:00", Name = "Muzyczny WOŚP", Localization = "MORiS ul. Dąbrowskiego 113"},
                 new TableCalendarData() {Time = "17:00 - 20:00", Name = "V. WOŚP w rytmie Zumby", Localization = "ZST nr 1 ul. Sportowa 23"},
             };
