e76656ee421e53a58e7cc156fc6e1e9e5da9690b
Adam Franz <hansik3333@gmail.com>
2021-12-13 23:23:33 +0100

    13.12.2021 23:23
    - Twórcy aplikacji (Linki i adresy e-mail)

--- GOCC/View/SponsorsPage.xaml.cs
+++ GOCC/View/SponsorsPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,12 @@ namespace GOCC.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SponsorsPage : ContentPage
     {
+        public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+
         public SponsorsPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
     }
 }
