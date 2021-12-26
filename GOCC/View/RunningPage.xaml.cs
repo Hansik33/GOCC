cd4e676a48f09a532f90bea5fc6b1c416e3fdb57
Artur <artur.rudzik@o2.pl>
2021-12-26 22:17:52 +0100

    RuningPageViewNModel added

--- GOCC/View/RunningPage.xaml.cs
+++ GOCC/View/RunningPage.xaml.cs
@@ -1,20 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using GOCC.ViewModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Diagnostics;
+
 
 namespace GOCC.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RunningPage : ContentPage
     {
+
+        RunningPageViewModel viewModel = new RunningPageViewModel();
         public RunningPage()
         {
+            BindingContext = viewModel;
             InitializeComponent();
+        }
+
+        private void Stop_clicked(object sender, EventArgs e)
+        {
+            
         }
     }
 }
---
+++ GOCC/ViewModel/RunningPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace GOCC.ViewModel
+{
+    internal class RunningPageViewModel : BindableObject
+    {
+        private string _time;
+        private string _distance;
+        public RunningPageViewModel()
+        {
+            _time = "00:00:00";
+            _distance = "0000";
+        }
+        public string Time
+        {
+            get { return _time; }
+            set { _time = value; OnPropertyChanged("Time"); }
+        }
+        public string Distance
+        {
+            get { return _distance; }
+            set { _distance = value; OnPropertyChanged("Distance"); }
+        }
+    }
+}
