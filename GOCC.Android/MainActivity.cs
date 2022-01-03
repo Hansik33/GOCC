8a1a979b2fa9d7de35a5e53d71a09feec3e407d0
Adam Franz <82848282+Hansik33@users.noreply.github.com>
2022-01-04 00:01:35 +0100

    04.01.2022 00:01
    Responsiveness

--- GOCC.Android/MainActivity.cs
+++ GOCC.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace GOCC.Droid
 {
-    [Activity(Label = "Bieg z sercem WOŚP", Icon = "@mipmap/appicon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Bieg z sercem WOŚP", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait, Icon = "@mipmap/appicon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
