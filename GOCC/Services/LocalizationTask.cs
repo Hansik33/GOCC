48f0c4c0a10837917ce0217ac0f9b2833a5edca6
Adam Franz <82848282+Hansik33@users.noreply.github.com>
2022-01-10 21:49:59 +0100

    Revert "Revert "Revert "ŻUREK  V.2"""
    This reverts commit cfbfb65a498a98364e639378878a7c3435f8075d.

--- GOCC/Services/LocalizationTask.cs
+++ GOCC/Services/LocalizationTask.cs
@@ -13,12 +13,7 @@ namespace GOCC.Services
         public double totaldistance = 0;
         double distanceToCheck = 0;
         double newdistance = 0;
-<<<<<<< Updated upstream
         public bool Stopping;
-=======
-
-        public bool Stopping = true;
->>>>>>> Stashed changes
 
         public LocalizationTask()
         {
@@ -27,7 +22,6 @@ namespace GOCC.Services
 
         public async Task Run(CancellationToken token)
         {
-<<<<<<< Updated upstream
             await Task.Run( async () =>
             {
                 Location prevlocation = await Geolocation.GetLocationAsync(new GeolocationRequest(GeolocationAccuracy.Best, TimeSpan.FromMinutes(1)));
@@ -79,42 +73,5 @@ namespace GOCC.Services
         }
 
         
-=======
-            await Task.Run(async () =>
-           {
-               Location prevlocation = await Geolocation.GetLocationAsync(new GeolocationRequest(GeolocationAccuracy.Best, TimeSpan.FromMinutes(1)));
-               await Task.Delay(1000);
-               while (Stopping)
-               {
-
-                       token.ThrowIfCancellationRequested();
-                       Location newlocation = await Geolocation.GetLocationAsync(new GeolocationRequest(GeolocationAccuracy.Best, TimeSpan.FromMinutes(1)));//pobranie danych
-                       newdistance = Location.CalculateDistance(newlocation, prevlocation, DistanceUnits.Kilometers);//obliczenie dystansu
-                       distanceToCheck = newdistance * 1000;
-
-                       if (distanceToCheck >= 11)//sprawdza czy nie ma błędu wynikającego z GPS
-                       {
-                           totaldistance += 0.003;//dodanie wartości poprawiającej po GPS ERROR
-                           var massage = new LocalizationMessage { Distance = totaldistance };
-                           Device.BeginInvokeOnMainThread(() =>
-                           {
-                               MessagingCenter.Send<LocalizationMessage>(massage, "Distance");
-                           });
-                       }
-                       else
-                       {
-                           totaldistance = totaldistance + newdistance;//dodanie do wyniku
-                           var massage = new LocalizationMessage { Distance = totaldistance };
-                           Device.BeginInvokeOnMainThread(() =>
-                           {
-                               MessagingCenter.Send<LocalizationMessage>(massage, "Distance");
-                           });
-                       }
-                       prevlocation = newlocation;
-                       await Task.Delay(1000);
-               }
-           },token);
-        }   
->>>>>>> Stashed changes
     }
 }
